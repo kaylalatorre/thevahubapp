f66f571807ac2937ff6a2c9ad687ae3c15d0f3eb
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2020-12-29 13:41:35 +0800

    added package.json, cleaning

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,7 @@
-scripts
+document.addEventListener('DOMContentLoaded', function() {
+	var calendarEl = document.getElementById('calendar');
+	var calendar = new FullCalendar.Calendar(calendarEl, {
+	  initialView: 'dayGridMonth'
+	});
+	calendar.render();
+});
