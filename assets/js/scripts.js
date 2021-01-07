c7a655091b90398b0ff9c1c534bafe045b6eb259
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-01-07 16:08:28 +0800

    set up 'hr-schedule'

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 	calendar.render();
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
