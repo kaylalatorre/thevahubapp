42bae7b0ae5cc5d3fef8fb105bac2df61b71de07
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2021-01-30 22:56:03 +0800

    fixing edit- and delete- class

--- assets/js/trainer-scripts.js
+++ assets/js/trainer-scripts.js
@@ -96,6 +96,91 @@ $(document).ready(function() {
 		}
 
 	});
+
+	$('.editClass').each(function() {
+		var classID = $('#classID').val();
+		var courseName = $('#courseName').val();
+        var dateToday = new Date();
+		var startDate = new Date($('#startDate').val());
+		var endDate = new Date($('#endDate').val());
+		var startTime = $('#startTime').val();
+		var endTime = $('#endTime').val();
+        var meetLink = $('#meetLink').val();
+        var classPhoto = $('#classPhoto').val();
+
+		// just to check to see if data is received
+		console.log(courseName, dateToday, startDate, endDate, startTime, endTime, meetLink);
+
+        // values for error-checking
+        var numDays = Math.round(endDate- startDate) / (1000 * 60 * 60 * 24) + 1;
+		var numHours = (calculateHours(startTime, endTime) * 0.1);
+
+		console.log(numDays, numHours);
+
+		var dateErrors = true,
+            timeErrors = true;
+            
+            // check if date fields are empty
+		if ((startDate == "Invalid Date") || (endDate == "Invalid Date") || (!startDate || !endDate)) {
+			if (startDate == "Invalid Date" || !startDate) alert('Set date.');
+		
+			if (endDate =="Invalid Date" || !startDate) alert('Set date.');
+		}
+			/* check if classes meet the criteria of eight (8) days and if selected start and end
+				dates are not earlier than today */
+		else if (startDate && endDate) {
+			if ((numDays != 8) || (startDate < dateToday) || (startDate > endDate)) {
+				if (numDays != 8) alert('Classes should last for eight (8) days.');
+
+				if (startDate < dateToday) alert('Date should not be earlier than today.');
+
+				if (startDate > endDate) alert('Start Date should be earlier than End Date.'); 
+			}
+			else dateErrors = false;
+		}
+		else dateErrors = false;
+		
+			// check if time fields are empty
+		if (!startTime || !endTime) {
+			if (!startTime) alert('Set time.');
+			if (!endTime) alert('Set time.');
+		}
+			/* check if submitted timeslot meets the criteria of six (6) hours a day and if selected
+				end time is not earlier than start time */
+		else if (startTime && endTime) {
+			if ((numHours != 6) || (startTime > endTime)) {
+				if (numHours != 6) alert('Classes should last for 6 hours a day.');
+
+				if (startTime > endTime) alert('Start Time should be earlier than End Time.');
+			}
+			else timeErrors = false;
+		}
+		else timeErrors = false;
+
+		// if no errors, submit to backend
+		if (!dateErrors && !timeErrors) {
+			console.log("no errors");
+			$.post('/edit-class', { classID: classID, courseName: courseName, startDate: startDate, endDate: endDate,
+				startTime: startTime, endTime: endTime, meetLink: meetLink, classPhoto: classPhoto}, function(res) {
+				switch (res.status){
+					case 200: {
+						alert("Class updated successfully.");
+						window.location.href = '/trainer-classes';
+                        break;
+					}
+					case 500: {
+						alert(res.mssg);
+						break;
+					}
+				}
+			});
+		}
+
+	}); 
+
+	$('.deleteClass').each(function() {
+
+	}); 
 	
 
 });
--- controller/tvhController.js
+++ controller/tvhController.js
@@ -616,39 +616,26 @@ const rendFunctions = {
 	
 	postEditClass: function(req, res) {
 		try{
-			let { courseName, startDate, endDate, startTime, endTime, meetLink, classPhoto } = req.body;
-			// console.log(courseName, startDate, endDate, startTime, endTime, meetLink)
-
-			// generate classID
-			var classID = generateClassID();
-			// console.log("ClassID: " + classID);
-
+			let { classID, courseName, startDate, endDate, startTime, endTime, meetLink, classPhoto } = req.body;
+	
 			var sTime = new Date("Jan 01 2021 " + startTime + ":00");
 			var eTime = new Date("Jan 01 2021 " + endTime + ":00");
 			// console.log(sTime, eTime);
 
-			// create the class
-			var tempClass = createClass(classID, req.session.user.userID, courseName, startDate, endDate, sTime, eTime, meetLink, classPhoto);
-
-			
-			// add into Class model
-			ClassDB.create(tempClass, function(error) {
-			if (error) {
-				res.send({status: 500, mssg: 'Error in adding class.'});
-				console.log("create-class error: " + error);
-			}
-			// add into TrainerInfo array
-			else {
-				res.send({status: 200});	
-				// UserDB.findOneAndUpdate({userID: req.session.user.userID},
-				// 	{$push: {TrainerInfo: tempClass}}, 
-				// 	{useFindAndModify: false}, function(err) {
-				// 		if (err) 
-				// 			res.send({status: 500, mssg: 'Cannot update Trainer Info'});
-				
-				// 		else res.send({status: 200});	
-				// 	});
-				}
+			ClassDB.findOneAndUpdate(
+				{ classID: classID },
+				{ $set: {
+					courseName: courseName, startDate: startDate, endDate: endDate,
+					startTime: startTime, endTime: endTime, meetLink: meetLink, classPhoto: classPhoto,
+				}},
+				{ useFindAndModify: false },
+				function(err, match) {
+					if (err) {
+						res.send({status: 500, mssg: "Error in updating class."});
+					}
+					else{
+						res.send({status: 200});
+					}
 			});
 		} catch(e){
 			res.send({status: 500, mssg: 'Cannot connect to db.'});
